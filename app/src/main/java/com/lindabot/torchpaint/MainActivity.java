cef6e37327933c522101f63a0c45116f1a52c4b7
LindaBot <voidnullbot@outlook.com>
2018-11-06 12:23:16 +1300

    Override volume behaviours within the app

--- app/src/main/java/com/lindabot/torchpaint/MainActivity.java
+++ app/src/main/java/com/lindabot/torchpaint/MainActivity.java
@@ -2,6 +2,8 @@ package com.lindabot.torchpaint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        super.onKeyUp(keyCode, event);
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
+        {
+            Toast.makeText(MainActivity.this,"Up",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+        {
+            Toast.makeText(MainActivity.this,"Up",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        super.onKeyDown(keyCode, event);
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
+        {
+            Toast.makeText(MainActivity.this,"Down",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+        {
+            Toast.makeText(MainActivity.this,"Down",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+
 }
